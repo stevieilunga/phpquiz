6ed8d9fc638d8ff23656704a5c8be31cec4ddb0a
Owner <owner@Owners-MacBook-Pro.local>
2019-03-28 09:32:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+            <style>
+                        body{
+                        background-color:#D2CCA1;
+                        margin:20px;
+                        padding:20px;
+                        margin-left:100px;
+                        margin-right:100px;
+                    }
+                    .frame{
+                        margin:40px;
+                        padding:20px;
+                        font-size: 24px;
+                        list-style-type: none;
+                    }
+                    .item{
+                        color: white;
+                        font-size: 34px;
+                        background-color:#45503B;
+                        margin: 20px;
+                        padding:20px;
+                    }
+                    li:hover{
+                        background-color:#8F5C38;
+                    }
+                    li{
+                        margin: 0 300px;
+                    }
+                    .title{
+                        text-align: center;
+                        font-size: 45px;
+                        color:#89023E;
+                        background-color:#F5CDA7;
+                    }
+
+
+            </style>
+            <title>Project</title>
+    </head>
+    <body>
+
+            <form action="index.php" method="post" id="quiz">
+                <h1 class="title">General Quiz</h1>
+                <fieldset class="frame">
+                <legend class="item">1.world's coldest desert Gobi belongs to which country?</legend>
+                <li><input type="radio" value="A" name="one">central african republic</li>
+                <li><input type="radio" value="B" name="one">mongolia</li>
+                <li><input type="radio" value="C" name="one">Indonesia</li>
+                <li><input type="radio" value="D" name="one">Egypt</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item"> 2. what is the official residence of the french president called?</legend>
+                <li><input type="radio" value="A" name="two">Elysee Palace </li>
+                <li><input type="radio" value="B" name="two">White house</li>
+                <li><input type="radio" value="C" name="two">Crystal palace</li>
+                <li><input type="radio" value="D" name="two">chateaux rouuge</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">3.which country is the largest consumer of Gold </legend>
+                <li><input type="radio" value="A" name="tree">Mexico</li>
+                <li><input type="radio" value="B" name="tree">USA</li>
+                <li><input type="radio" value="C" name="tree">England</li>
+                <li><input type="radio" value="D" name="tree">India</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">4. where in the world is the headquarters of INTERPOL?</legend>
+                <li><input type="radio" value="A" name="four">Lyon, France</li>
+                <li><input type="radio" value="B" name="four">Rome, Itali</li>
+                <li><input type="radio" value="C" name="four">Russia</li>
+                <li><input type="radio" value="D" name="four">Belgium</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">5. which country in the world ranks top in the production of coconut?</legend>
+                <li><input type="radio" value="A" name="five">USA</li>
+                <li><input type="radio" value="B" name="five">tanzania</li>
+                <li><input type="radio" value="C" name="five">Philippines</li>
+                <li><input type="radio" value="D" name="five">mozambique</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">6.who developed the first computer languages BASIC?</legend>
+                <li><input type="radio" value="A" name="six">Albert Einstein</li>
+                <li><input type="radio" value="B" name="six">John G. Kemeny and Thomas E. Kurtz </li>
+                <li><input type="radio" value="C" name="six">Isaac Newton</li>
+                <li><input type="radio" value="D" name="six">Bill Gate</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">7.what is known as "Perfume Ball" in cricket? </legend>
+                <li><input type="radio" value="A" name="seven"></li>
+                <li><input type="radio" value="B" name="seven">The bounced ballat the height of nose-level of batsman</li>
+                <li><input type="radio" value="C" name="seven"></li>
+                <li><input type="radio" value="D" name="seven"> </li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">8.which is the purest water of nature?</legend>
+                <li><input type="radio" value="A" name="eight">Rain Water</li>
+                <li><input type="radio" value="B" name="eight">Dam Water</li>
+                <li><input type="radio" value="C" name="eight">Storm Water</li>
+                <li><input type="radio" value="D" name="eight">Borehole Water </li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">9.what is "Hey You?</legend>
+                <li><input type="radio" value="A" name="nine">A way of greeting</li>
+                <li><input type="radio" value="B" name="nine">A way of calling a friend</li>
+                <li><input type="radio" value="C" name="nine">salutation</li>
+                <li><input type="radio" value="D" name="nine">A Computer Virus</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">10.which country is known as the "Economic Power House of Europe"?</legend>
+                <li><input type="radio" value="A" name="ten">England</li>
+                <li><input type="radio" value="B" name="ten">France</li>
+                <li><input type="radio" value="C" name="ten">Russia</li>
+                <li><input type="radio" value="D" name="ten">Germany</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">11.Who wrote "A Brief History of Time"?</legend>
+                <li><input type="radio" value="A" name="eleven">William Shakespeare</li>
+                <li><input type="radio" value="B" name="eleven">Steve Harvey</li>
+                <li><input type="radio" value="C" name="eleven">Stephen Hawking</li>
+                <li><input type="radio" value="D" name="eleven">Martin Luther King</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">12.who are the biggest producers of Cement in the World?</legend>
+                <li><input type="radio" value="A" name="twelve">Afrisam</li>
+                <li><input type="radio" value="B" name="twelve">cilu</li>
+                <li><input type="radio" value="C" name="twelve">Lafarge</li>
+                <li><input type="radio" value="D" name="twelve">cementation</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">13.Which country has the World's one-fourth oil reserve?</legend>
+                <li><input type="radio" value="A" name="thirteen">Angola</li>
+                <li><input type="radio" value="B" name="thirteen">Iraq</li>
+                <li><input type="radio" value="C" name="thirteen">Saudi Arabia</li>
+                <li><input type="radio" value="D" name="thirteen">Mexico</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">14.who prepared the intelligence test first? </legend>
+                <li><input type="radio" value="A" name="fourteen">Einstein</li>
+                <li><input type="radio" value="B" name="fourteen">Alfred Binet</li>
+                <li><input type="radio" value="C" name="fourteen">Newton</li>
+                <li><input type="radio" value="D" name="fourteen">Pythagorus</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">15.which are the official languages of Switzerland?</legend>
+                <li><input type="radio" value="A" name="fifteen">German, French and Italian</li>
+                <li><input type="radio" value="B" name="fifteen">Spanish, English, French</li>
+                <li><input type="radio" value="C" name="fifteen">Portuguese and Flemish</li>
+                <li><input type="radio" value="D" name="fifteen">English and Flemish</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">16.Who discovered Richter Scale? </legend>
+                <li><input type="radio" value="A" name="sixteen">Nelson Mandela</li>
+                <li><input type="radio" value="B" name="sixteen">John Kenedy</li>
+                <li><input type="radio" value="C" name="sixteen">Charles F. Richter</li>
+                <li><input type="radio" value="D" name="sixteen">Pythagorus </li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">17.what is the National emblem of New Zealand??</legend>
+                <li><input type="radio" value="A" name="seventeen">Protea</li>
+                <li><input type="radio" value="B" name="seventeen">Kiwi</li>
+                <li><input type="radio" value="C" name="seventeen">Mango</li>
+                <li><input type="radio" value="D" name="seventeen">Springbok</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">18.Name the first monkey born on genetic technology</legend>
+                <li><input type="radio" value="A" name="eighteen">Andrew</li>
+                <li><input type="radio" value="B" name="eighteen">Eddi</li>
+                <li><input type="radio" value="C" name="eighteen">ANDI</li>
+                <li><input type="radio" value="D" name="eighteen">Pedro</li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">19.Where is the longest highway tunnel of the world situated? </legend>
+                <li><input type="radio" value="A" name="nineteen">Norway</li>
+                <li><input type="radio" value="B" name="nineteen">Spain</li>
+                <li><input type="radio" value="C" name="nineteen">South Africa</li>
+                <li><input type="radio" value="D" name="nineteen">Dubai </li>
+                </fieldset>
+                <fieldset class="frame">
+                <legend class="item">20.what is the capital city of the Democratic Republic of Congo?</legend>
+                <li><input type="radio" value="A" name="twenty">Durban</li>
+                <li><input type="radio" value="B" name="twenty"> Libreville</li>
+                <li><input type="radio" value="C" name="twenty">Accra</li>
+                <li><input type="radio" value="D" name="twenty">Kinshasa</li>
+                </fieldset>
+                <input type="submit">
+                
+            
+            </form>
+
+
+    </body>
+</html>
+
+<?php
+
+if ($_POST && count($_POST) ==20){
+    $ans1=$_POST['one'];
+    $ans2=$_POST['two'];
+    $ans3=$_POST['tree'];
+    $ans4=$_POST['four'];
+    $ans5=$_POST['five'];
+    $ans6=$_POST['six'];
+    $ans7=$_POST['seven'];
+    $ans8=$_POST['eight'];
+    $ans9=$_POST['nine'];
+    $ans10=$_POST['ten'];
+    $ans11=$_POST['eleven'];
+    $ans12=$_POST['twelve'];
+    $ans13=$_POST['thirteen'];
+    $ans14=$_POST['fourteen'];
+    $ans15=$_POST['fifteen'];
+    $ans16=$_POST['sixteen'];
+    $ans17=$_POST['seventeen'];
+    $ans18=$_POST['eighteen'];
+    $ans19=$_POST['nineteen'];
+    $ans20=$_POST['twenty'];
+
+
+   $totalCorrect = 0;
+
+        if($ans1 =="A"){$totalCorrect++;}
+        if($ans2 =="A"){$totalCorrect++;}
+        if($ans3 =="D"){$totalCorrect++;}
+        if($ans4 =="A"){$totalCorrect++;}
+        if($ans5 =="C"){$totalCorrect++;}
+        if($ans6 =="B"){$totalCorrect++;}
+        if($ans7 =="B"){$totalCorrect++;}
+        if($ans8 =="A"){$totalCorrect++;}
+        if($ans9 =="D"){$totalCorrect++;}
+        if($ans10 =="D"){$totalCorrect++;}
+        if($ans11 =="C"){$totalCorrect++;}
+        if($ans12 =="C"){$totalCorrect++;}
+        if($ans13 =="C"){$totalCorrect++;}
+        if($ans14 =="B"){$totalCorrect++;}
+        if($ans15 =="A"){$totalCorrect++;}
+        if($ans16 =="C"){$totalCorrect++;}
+        if($ans17 =="B"){$totalCorrect++;}
+        if($ans18 =="C"){$totalCorrect++;}
+        if($ans19 =="A"){$totalCorrect++;}
+        if($ans20 =="D"){$totalCorrect++;}
+
+
+        echo "YOUR TOTAL SCORES IS " .$totalCorrect ."/20";
+    }
+    elseif ($_POST && count($_POST)<20) {?>
+        <h1 class="result"><? echo "YOU DID'NT ANSWER ALL THE QUESTION";?></h1>
+    <?}
+    ?>
+
